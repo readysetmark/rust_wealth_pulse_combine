56a5003c8964e9066f8871e3bab9bd1860728cb1
Mark Williams <mark@readysetmark.com>
2015-10-18 22:28:49 -0500

    Added line_number
    Added `line_number` “parser” and test.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 extern crate combine;
 
-use combine::{char, digit, many, parser, Parser, ParserExt, ParseResult, ParseError, State};
+use combine::{char, digit, many, parser, Parser, ParserExt, ParseResult, ParseError};
 use combine::combinator::FnParser;
-use combine::primitives::Stream;
+use combine::primitives::{Consumed, State, Stream};
 
 #[derive(PartialEq, Debug)]
 struct Date {
@@ -10,6 +10,22 @@ struct Date {
 	month: i32,
 	day: i32
 }
+
+
+
+/// Gets the current line number.
+fn line_number<I>(input: State<I>) -> ParseResult<i32, I>
+where I: Stream<Item=char> {
+	Result::Ok((input.position.line, Consumed::Empty(input)))
+}
+
+#[test]
+fn line_number_test() {
+	let (line_num, remaining_input) = parser(line_number).parse("hello").unwrap();
+	assert_eq!(line_num, 1);
+	assert_eq!(remaining_input, "hello");
+}
+
 
 
 /// Takes a tuple of digit characters and converts them to an i32
@@ -25,7 +41,6 @@ fn two_digits_to_int_test() {
 	let result = two_digits_to_int(two_digits);
 	assert_eq!(result, 27);
 }
-
 
 /// Wrapped parser for parsing two digits. e.g. 17
 fn two_digits<I>() -> FnParser<I, fn (State<I>) -> ParseResult<i32, I>>
@@ -47,6 +62,8 @@ fn two_digits_test() {
 	let (result, _remaining_input) = parse_result.unwrap();
 	assert_eq!(result, 9);
 }
+
+
 
 /// Parses a date. e.g. 2015-10-17
 fn date<I>(input: State<I>) -> ParseResult<Date, I>
@@ -70,6 +87,7 @@ fn date_test() {
 	assert_eq!(result.month, 10);
 	assert_eq!(result.day, 17);
 }
+
 
 
 fn main() {
